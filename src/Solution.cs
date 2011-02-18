9d9abc0438b9b3d162ccecaddb9037de16919afe
remi <remi@remitaylor.com>
2011-02-18 06:04:27 -0700

    Working on Solution parsing ... want to pull in IFile before continuing ...

--- src/Solution.cs
+++ src/Solution.cs
@@ -5,14 +5,69 @@ using System.Collections.Generic;
 
 namespace NVS {
 
+	// TODO implmenet IFile?  Need to make a project and pull in IFile and IDirectory
+
 	/// <summary>Represents a .sln solution file</summary>
 	public class Solution {
 
 		public Solution() {}
 		public Solution(string path) {
-			
+			Path = path;
 		}
 
-		public virtual List<Project> Projects { get { return null; } }
+		string _path;
+		List<Project> _projects;
+		List<Section> _sections;
+
+		/// <summary>The file system path to this .sln file</summary>
+		public virtual string Path {
+			get { return _path; }
+			set {
+				_projects = null;
+				_sections = null;
+				_path     = value;
+			}
+		}
+
+		/// <summary>All of the Project found in this Solution</summary>
+		public virtual List<Project> Projects {
+			get {
+				if (_projects == null) Parse();
+				return _projects;
+			}
+		}
+
+		/// <summary>All of the GlobalSection sections found in this Solution</summary>
+		public virtual List<Section> Sections {
+			get {
+				if (_sections == null) Parse();
+				return _sections;
+			}
+		}
+
+		void Parse() {
+			if (this.DoesNotExist()) return;
+
+			Sections = new List<Section>();
+			Projects = new List<Project>();
+
+			foreach (var line in this.Lines())
+				if (line.StartsWith("Project("))
+					Projects.Add(ProjectFromLine(line));
+				else if (line.TrimStart().StartsWith("GlobalSection("))
+					Sections.Add(SectionFromLine(line));
+				else if (Sections.Count > 0 && ! line.TrimStart().StartsWith("EndGlobal"))
+					Sections.Last().Text += "\n" + line;
+		}
+
+		// Project("{GUI}") = "MyApp", "MyApp\MyApp.csproj", "{GUID}"
+		Project ProjectFromLine(string line) {
+			return new Project();
+		}
+
+		// GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		Section SectionFromLine(string line) {
+
+		}
 	}
 }
