944ef5b8bf59359014a2184ade450bdb591f65e8
remi <remi@remitaylor.com>
2011-04-18 05:18:40 -0700

    Added some TODO notes ... cleaned up root directoy of project ...

--- build-and-test-release.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-find . -name obj -type d -exec rm -rfv {} \;
-find . -name bin -type d -exec rm -rfv {} \;
-rm -rf TestResult.xml
-xbuild /p:Configuration=Release
-nunit-color-console -labels "$@" bin/Release/Clide.Specs.dll
